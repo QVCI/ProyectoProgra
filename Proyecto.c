1a0bbff40154ddc392fa8bfdf0f05cd187aeddd1
QVCI <carloivan2910@gmail.com>
2025-11-02 13:11:31 -0600

    base logica del menu principal

--- Proyecto.c
+++ Proyecto.c
@@ -10,26 +10,58 @@ int main()
 {
     int opc;
     bool DirMemoriaLista = false;
+    bool ArchvioLeido = false;
     do
     {
         LimpiarPantalla();
         
         printf("Menu\n");
-        if (opc == -1)
-        {
-            printf("Seleccione una opcion valida\n");
-        }
+       
+        
         printf("0.- Modificar direccion de los archivos\n");
         printf("1.- Leer Archivo (Infijo) y guardar (Postfijo)\n");
         printf("2.- Evaluar archivo (Postfijo)\n");
         printf("3.- Salir\n");
+         if (opc == -1)
+        {
+            printf("Seleccione una opcion valida\n");
+        }
+        if (opc == -2)
+        {
+            printf("Primero Seleccione la direccion de los archivos.txt (Opcion 0)\n");
+            
+        }
+         if (opc == -3)
+        {
+            printf("Primero se debe leer el archivo y guardar (Opcion 1)\n");
+            
+        }
+        printf("Ingrese una opcion: ");
         scanf("%d", &opc);
         switch(opc){
             case 0:
+                DirMemoriaLista = true;
             break;
             case 1:
+                if(!DirMemoriaLista)
+                {
+                    opc = -2;
+                    break;
+                }
+                ArchvioLeido = true;
+
             break;
             case 2:
+                if(!DirMemoriaLista)
+                {
+                    opc = -2;
+                    break;
+                }
+                if(!ArchvioLeido)
+                {
+                    opc = -3;
+                    break;
+                }
             break;
             case 3:
             break;
